3666d7fb201e227a1c74df297ecf262e13118053
Aga <149872643+Agvea@users.noreply.github.com>
2025-04-07 22:46:05 +0200

    Update firebase-config.js

--- Warsztat-3D/JS/firebase-config.js
+++ Warsztat-3D/JS/firebase-config.js
@@ -1,24 +1,24 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyC6gOxKBrkwKKoi3B-ihOayNSDYhebhZ74",
-    authDomain: "warsztat-3d.firebaseapp.com",
-    projectId: "warsztat-3d",
-    storageBucket: "warsztat-3d.firebasestorage.app",
-    messagingSenderId: "405984174379",
-    appId: "1:405984174379:web:e74aa5aa5297ebfa20256e",
-    measurementId: "G-2X1Z66HYJJ"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6gOxKBrkwKKoi3B-ihOayNSDYhebhZ74",
+  authDomain: "warsztat-3d.firebaseapp.com",
+  databaseURL: "https://warsztat-3d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "warsztat-3d",
+  storageBucket: "warsztat-3d.firebasestorage.app",
+  messagingSenderId: "405984174379",
+  appId: "1:405984174379:web:e74aa5aa5297ebfa20256e",
+  measurementId: "G-2X1Z66HYJJ"
+};
 
-// Inicjalizacja Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
-// Eksportuj tylko to, co potrzebujesz
 export { db };
